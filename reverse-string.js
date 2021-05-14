3223280a9466c11ac3292356bc17fa0ac652a321
Reuven <reuvenroth@users.noreply.github.com>
2021-05-13 23:34:46 -0400

    Update reverse-string.js
    Added if/else handler for cancel button on initial string input prompt.

--- reverse-string.js
+++ reverse-string.js
@@ -1,14 +1,19 @@
 reverseString = () => {
-  userInput = prompt('Say it in reverse!', 'Reverse your thoughts:');
-  result = alert("Your message reversed is: "
-    + userInput.split("").reverse().join(""));
-  let newUserInput = confirm("Would you like to reverse another message?");
-  if (newUserInput == true) {
-    reverseString();
-  }
-  else {
+  userInput = prompt('Say it in reverse!', 'Reverse your thoughts');
+  if (userInput === null) {
     alert("See you next time!");
-  };
+  }
+  else{
+    result = alert("Your message reversed is: "
+      + userInput.split("").reverse().join(""));
+    let newUserInput = confirm("Would you like to reverse another message?");
+    if (newUserInput == true) {
+      reverseString();
+    }
+    else {
+      alert("See you next time!");
+    }
+  }
 };
 
 reverseString();
